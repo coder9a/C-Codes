e25dd30dc3176940836724e5856bbac0de7fa3d7
Raj Balhara <42884781+coder9a@users.noreply.github.com>
2021-03-24 16:24:09 +0530

    Add files via upload

---
+++ Check_Prime_Number.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+	int n,i,flag=0;
+	printf("Enter number\t");
+	scanf("%d",&n);
+	for(i=2;i<=n/2;i++)
+	{
+		if(n%i==0)
+		{
+			flag = 1;
+			break;
+		}
+	}
+	if(flag == 1)
+		{
+			printf("Not prime");
+			//return;
+		}
+		else
+		{
+			printf("Prime");
+			//return;
+		}
+	return 0;
+}
